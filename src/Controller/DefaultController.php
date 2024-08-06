1c96bdfc5ac7aa0bdd2dca6f6558b3f37a4fb046
he-lau <he.laurent.1130@gmail.com>
2024-08-06 19:00:15 +0200

    Prise en compte de la pagination pour le scraping du catalogue entier de scrapeme.live + CSV

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,45 +12,73 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class DefaultController extends AbstractController
 {
-
-    private $client;
     private $browser;
 
     public function __construct()
     {
-        $this->client = HttpClient::create();
         $this->browser = new HttpBrowser(HttpClient::create());
 
     }
 
-    private function scrapingScrapeMe() {
-        // https://www.zenrows.com/blog/goutte-web-scraping#extract-one-element
-        
-        $crawler = $this->browser->request('GET', 'https://scrapeme.live/shop/');
-        // get the response returned by the server
-        $response = $this->browser->getResponse();
-        // extract the HTML content and print it
-        $html = $response->getContent();
+
+    private function scrapingScrapeMe($pageCounter=1,$pageLimit=99) {
+        // https://www.zenrows.com/blog/goutte-web-scraping
+
+        // the first page to visit in the crawling logic
+        $firstPageToScrape = "https://scrapeme.live/shop/page/1/";
+
+        // the Set of pages discovered during the crawling logic
+        $pagesDiscovered = [$firstPageToScrape];
+        // the list of remaining pages to scrape
+        $pagesToScrape = [$firstPageToScrape]; 
 
         $products = [];
 
-        $crawler->filter("li.product")->each(function ($productHTMLElement) use (&$products) {
-            // scraping logic
-            $url = $productHTMLElement->filter("a")->eq(0)->attr("href");
-            $image = $productHTMLElement->filter("img")->eq(0)->attr("src");
-            $name = $productHTMLElement->filter("h2")->eq(0)->text();
-            $price = $productHTMLElement->filter("span")->eq(0)->text();
+        while (count($pagesToScrape) != 0 && $pageCounter <= $pageLimit) {
+            // first elemnent of the queue
+            $pageUrl = array_shift($pagesToScrape);
+    
+            //dump($pageUrl);
         
-            // instantiate a new product object
-            $product = [
-                "url" => $url,
-                "image" => $image,
-                "name" => $name,
-                "price" => $price
-            ];
-            // add it to the list
-            $products[] = $product;
-        });
+            // get current 
+            $crawler = $this->browser->request("GET", $pageUrl);
+        
+            // Crawling logic to find and queue the "Next" page link
+            $crawler->filter("a.page-numbers.next")->each(function ($paginationHTMLElement) use (&$pagesDiscovered, &$pagesToScrape) {
+                // Extract the "Next" page URL
+                $nextPageLink = $paginationHTMLElement->attr("href");
+                //dump($nextPageLink);
+
+                // Check if the next page link has already been discovered
+                if (!in_array($nextPageLink, $pagesDiscovered)) {
+                    // Add it to the queue to scrape and mark it as discovered
+                    $pagesToScrape[] = $nextPageLink;
+                    $pagesDiscovered[] = $nextPageLink;
+                }
+            });
+
+        
+            // scraping logic
+            $crawler->filter("li.product")->each(function ($productHTMLElement) use (&$products) {
+                $url = $productHTMLElement->filter("a")->eq(0)->attr("href");
+                $image = $productHTMLElement->filter("img")->eq(0)->attr("src");
+                $name = $productHTMLElement->filter("h2")->eq(0)->text();
+                $price = $productHTMLElement->filter("span")->eq(0)->text();
+        
+                // instantiate a new product object
+                $product = [
+                    "url" => $url,
+                    "image" => $image,
+                    "name" => $name,
+                    "price" => $price
+                ];
+                // add it to the list
+                $products[] = $product;
+            });
+        
+            // increment the iterator counter
+            $pageCounter++;
+        }
 
         return $products;
     }
